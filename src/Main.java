508d0499715f891197684452c5907baf7b3b4252
Alina Shirikhanova <alina.meller.98@mail.ru>
2023-04-13 20:51:49 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Учимся пользоваться гитом в среде разработки");
+        System.out.println("Учимся отслеживать коммиты на гитхаб");
     }
 }
